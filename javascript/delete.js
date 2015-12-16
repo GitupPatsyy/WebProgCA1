c2c61ec398cbaf262d5d930e83e25a2628ac67ab
GitupPatsyy <roryplunkettboyle12@gmail.com>
2015-12-16 18:06:20 +0000

    Delete button works

--- javascript/delete.js
+++ javascript/delete.js
@@ -1,13 +1,12 @@
 window.onload = function () {
-    var deleteData = document.getElementByClassName("deletebtn");
+    var deleteData = document.getElementsByClassName("deletebtn");
 
     for (var i = 0; i < deleteData.length; i++) {
         deleteData[i].addEventListener("click", function (event) {
+            
             if (!confirm("Do you really want to Delete this error?")) {
                 event.preventDefault();
             }
-
-
         });
     }
 };
